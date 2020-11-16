204dacd6258ae6e99c639687ae8c2936d1e517cd
sahilkashyap78 <sahilkashyap78@gmail.com>
2020-11-16 12:40:43 +0530

    Ui Camera created

---
+++ AwesomeKnight/Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Image fillWaitImage_1;
+    public Image fillWaitImage_2;
+    public Image fillWaitImage_3;
+    public Image fillWaitImage_4;
+    public Image fillWaitImage_5;
+    public Image fillWaitImage_6;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
